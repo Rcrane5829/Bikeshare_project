2d37bb668694f0f0c5336ab09529f8a1327b6493
Ryan Crane <Rcrane5829@gmail.com>
2023-07-02 15:46:14 -0400

    Changed a #todo

--- bikeshare.py
+++ bikeshare.py
@@ -82,7 +82,7 @@ def time_stats(df):
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-    # TO DO: display the most common month
+    # displays the most common month
 
 
     common_month= df['month'].mode()[0]
